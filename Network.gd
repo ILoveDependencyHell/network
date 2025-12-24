cd719a7c67fe56ae8a9a6c7efcc7d61d90a0a931
Liam <git@liamsherwin.com>
2025-12-24 21:06:53 +1300

    init

--- Network.gd
+++ Network.gd
@@ -1,5 +1,10 @@
 class_name Network extends Node
 
-func hello_network(p_source: String) -> void:
+
+func _init() -> void:
+	hello_network("Network.init()")
+
+
+static func hello_network(p_source: String) -> void:
 	prints(p_source, "> Hello, Network!")
 	Utils.hello_utils(p_source + " > Network")
